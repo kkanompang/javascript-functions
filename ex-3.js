e0ac0b7c8843d58a6546c34f297dfd67d94b6d5e
Kanompang <suparadajk21@gmail.com>
2024-09-15 22:42:48 +0700

    BP5-Kanompang

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,12 @@
 // Start coding here
+let subtract = (a, b) => a - b;
+subtract = subtract(10, 5);
+console.log(subtract);
+
+let multiply = (a, b) => a*b;
+multiply = multiply(10, 5);
+console.log(multiply);
+
+let divide = (a, b) => a/b;
+divide = divide(10, 5);
+console.log(divide);
